be9569582bc010b70166171007eebd3475922002
KIM SEONGJU <80195353+wing-tree@users.noreply.github.com>
2023-05-22 16:59:36 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,6 +52,8 @@ else
   fi
 fi
 
+echo "--reporter: $CUSTOM_RULE_PATH $RELATIVE $ANDROID $BASELINE $INPUT_FILE_GLOB"
+
 ktlint --reporter=checkstyle $CUSTOM_RULE_PATH $RELATIVE $ANDROID $BASELINE $INPUT_FILE_GLOB |
   reviewdog -f=checkstyle \
     -name="${INPUT_NAME}" \
